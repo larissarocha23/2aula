3ad4bf52155c6e690505613caff0ad15d7553b77
larissarocha23 <lr974443@gmail.com>
2024-09-09 14:28:11 +0000

    aula09/09

---
+++ aula23.java
@@ -0,0 +1,10 @@
+public class Livro{
+    public livro (double preco, String titulo){
+        this.preco = preco;
+        this.titulo = titulo
+        
+    public class projetoLivraria{
+        public static void main (String[] args){
+            Livro obj = new  Livro(44.9, "Harry Ptter...");
+ 
+    }
