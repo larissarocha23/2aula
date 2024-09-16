9a7c2d180d90568d12575f49559160d2ad8a23ce
larissarocha23 <lr974443@gmail.com>
2024-09-16 14:30:45 +0000

    ProjetoUniversidade

---
+++ br/unama/projetoUniversidade/ProjetoUniversidade.java
@@ -0,0 +1,8 @@
+public  class ProjetoUniversidade{
+    public static void main(String args[])
+    {
+        Aluno aluno2 = new Aluno();
+        Professor prof2 = new Professor();
+        Pessoa Pessoa1 = new Pessoa();
+    }
+}
